753bae203267de477969d7d2d8023768387077b4
Mathieu Clement <mclement2@dons.usfca.edu>
2017-11-10 14:42:22 -0800

    Bring mack the servo, delete partial file

--- arduino/rotary_encoder_and_servo/rotary_encoder_and_servo.ino
+++ arduino/rotary_encoder_and_servo/rotary_encoder_and_servo.ino
@@ -1,51 +1,57 @@
+#include "Servo.h"
+
 #define rotaryA 2
- #define rotaryB 3
- #define LED 13
+#define rotaryB 3
+#define LED 13
 
- volatile int stateA = LOW;
- volatile int stateB = LOW;
- volatile int counter = 0;
- volatile int lastCounter = 0;
+volatile int stateA = LOW;
+volatile int stateB = LOW;
+volatile int counter = 0;
+volatile int lastCounter = 0;
 
- void setup() { 
-   pinMode (rotaryA, INPUT_PULLUP);
-   pinMode (rotaryB, INPUT_PULLUP);
-   pinMode (LED, OUTPUT);
+void setup() { 
+  pinMode (rotaryA, INPUT_PULLUP);
+  pinMode (rotaryB, INPUT_PULLUP);
+  pinMode (LED, OUTPUT);
 
-   Serial.begin (9600);
+  Serial.begin (9600);
 
-   attachInterrupt(digitalPinToInterrupt(rotaryA), changeA, CHANGE);
-   attachInterrupt(digitalPinToInterrupt(rotaryB), changeB, CHANGE);
+  Servo servo;
+  servo.attach(9);
+  servo.write(180); // max speed. 90 to stop, 0 for max speed in reverse
 
-   stateA = digitalRead(rotaryA);
-   stateB = digitalRead(rotaryB);
- } 
- void loop() {
-   digitalWrite(LED, stateA);
+  attachInterrupt(digitalPinToInterrupt(rotaryA), changeA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(rotaryB), changeB, CHANGE);
 
-   if (counter != lastCounter) {
+  stateA = digitalRead(rotaryA);
+  stateB = digitalRead(rotaryB);
+} 
+void loop() {
+  digitalWrite(LED, stateA);
+
+  if (counter != lastCounter) {
     Serial.println(counter);
     lastCounter = counter;
-   }
- }
+  }
+}
 
- void changeA() {
-   stateA = !stateA;
+void changeA() {
+  stateA = !stateA;
 
-   if (stateA == HIGH) {
-     if (stateB == LOW ) {
-       counter++;
-       if (counter == 1000) {
-         counter = 0;
-       }
-     } else {
-         counter--;
-         if (counter == -1) {
-           counter = 999;
-         }
-     }
- }
+  if (stateA == HIGH) {
+    if (stateB == LOW ) {
+      counter++;
+      if (counter == 1000) {
+        counter = 0;
+      }
+    } else {
+        counter--;
+        if (counter == -1) {
+          counter = 999;
+        }
+    }
+}
 
- void changeB() {
-   stateB = !stateB;
- }
+void changeB() {
+  stateB = !stateB;
+}
