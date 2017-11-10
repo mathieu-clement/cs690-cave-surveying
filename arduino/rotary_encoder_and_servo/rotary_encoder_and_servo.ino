f9d3f845e7b3b30c38d5a5c07c4cb5d4db13f3b6
Mathieu Clement <mclement2@dons.usfca.edu>
2017-11-10 11:52:07 -0800

    Wrap up around 1000, correct direction, set servo to full speed

---
+++ arduino/rotary_encoder_and_servo/rotary_encoder_and_servo.ino
@@ -0,0 +1,62 @@
+#include <Servo.h> 
+
+ #define outputA 2
+ #define outputB 3
+ #define LED 13
+ 
+ volatile int stateA = LOW;
+ volatile int stateB = LOW;
+ volatile int counter = 0;
+ volatile int lastCounter = 0;
+ 
+ void setup() { 
+   pinMode (outputA, INPUT_PULLUP);
+   pinMode (outputB, INPUT_PULLUP);
+   // pinMode (outputB, INPUT);
+   pinMode (LED, OUTPUT);
+   
+   Serial.begin (9600);
+
+   Servo servo;
+   servo.attach(9);
+   //servo.write(180);
+
+   attachInterrupt(0 /* pin 2 */, changeA, CHANGE);
+   attachInterrupt(1 /* pin 3 */, changeB, CHANGE);
+
+   stateA = digitalRead(outputA);
+   stateB = digitalRead(outputB);
+ } 
+ void loop() {
+   //state = digitalRead(outputA);
+   digitalWrite(LED, stateA);
+
+   if (counter != lastCounter) {
+    Serial.println(counter);
+    lastCounter = counter;
+   }
+   //Serial.println(state);
+ }
+
+ void changeA() {
+   stateA = !stateA;
+
+  if (stateA == HIGH && stateB == HIGH) {
+      counter++;
+      if (counter == 1000) {
+        counter = 0;
+      }
+      
+   }   
+   if (stateA == HIGH && stateB == LOW) {
+      counter--;
+      if (counter == -1) {
+        counter = 999;
+      }
+   }
+ }
+
+ void changeB() {
+   stateB = !stateB;
+ }
+
