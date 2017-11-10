f1819a3b2e6af70286292be26179a63534829784
Mathieu Clement <mclement2@dons.usfca.edu>
2017-11-10 10:34:45 -0800

    Reading from rotary encoder (200 pulses by rotation)

---
+++ arduino/rotary_encoder/rotary_encoder.ino
@@ -0,0 +1,49 @@
+ #define outputA 2
+ #define outputB 3
+ #define LED 13
+ 
+ volatile int stateA = LOW;
+ volatile int stateB = LOW;
+ volatile int counter = 0;
+ volatile int lastCounter = 0;
+ 
+ void setup() { 
+   pinMode (outputA, INPUT_PULLUP);
+   pinMode (outputB, INPUT_PULLUP);
+   // pinMode (outputB, INPUT);
+   pinMode (LED, OUTPUT);
+   
+   Serial.begin (9600);
+
+   attachInterrupt(0 /* pin 2 */, changeA, CHANGE);
+   attachInterrupt(1 /* pin 3 */, changeB, CHANGE);
+
+   stateA = digitalRead(outputA);
+   stateB = digitalRead(outputB);
+ } 
+ void loop() {
+   //state = digitalRead(outputA);
+   digitalWrite(LED, stateA);
+
+   if (counter != lastCounter) {
+    Serial.println(counter);
+    lastCounter = counter;
+   }
+   //Serial.println(state);
+ }
+
+ void changeA() {
+   stateA = !stateA;
+
+  if (stateA == HIGH && stateB == HIGH) {
+      counter++; // clockwise
+   }   
+   if (stateA == HIGH && stateB == LOW) {
+      counter--; // clockwise
+   }
+ }
+
+ void changeB() {
+   stateB = !stateB;
+ }
+
