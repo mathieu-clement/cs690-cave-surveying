620fe8830fd204a306c5326bb19dcf0b75621d3e
Mathieu Clement <mclement2@dons.usfca.edu>
2017-12-02 17:02:58 -0800

    Write to file, progress bar

--- client/read_serial.py
+++ client/read_serial.py
@@ -1,9 +1,15 @@
-#!/usr/bin/env python3.4
+#!/usr/bin/env python3.6
 
 import glob
+import logging
 import serial
 import sys
 import time
+import tqdm
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 def arduino_is_ready(arduino_output):
     return arduino_output == b'Ready\r\n'
@@ -11,30 +17,52 @@ def arduino_is_ready(arduino_output):
 if not sys.platform.startswith('darwin'):
     sys.exit('Only MacOS is supported at this time. Program will now terminate.')
 
-serial_ports = glob.glob('/dev/tty.usbmodem*')
-print('Found USB modems:', serial_ports)
-assert len(serial_ports) == 1, "More than one USB modem found."
+baudrate=115200
 
-print('Opening ', serial_ports[0])
-ser = serial.Serial(serial_ports[0])
-print('Using serial port', ser.name)
+if len(sys.argv) == 3:
+    ser = serial.Serial(sys.argv[2],baudrate=baudrate)
+else:
+    serial_ports = glob.glob('/dev/tty.usbmodem*')
+    logger.info('Found USB modems: %s', serial_ports)
+    assert len(serial_ports) == 1, "More than one USB modem found."
 
-print('Waiting for "Ready" signal from Arduino')
+    logger.info('Opening %s', serial_ports[0])
+    ser = serial.Serial(serial_ports[0], baudrate=baudrate)
+
+logger.info('Using serial port %s', ser.name)
+
+logger.info('Waiting for "Ready" signal from Arduino')
 possibly_ready_str = ser.readline()
 assert arduino_is_ready(possibly_ready_str), 'Expected to receive Ready from Arduino but received ' + str(possibly_ready_str) + ' instead'
 
-print('Sending "sweep" command')
+logger.info('Sending "sweep" command')
 ser.write(b'sweep')
 ser.flush()
 
-while ser.is_open:
-    line = ser.readline()
-    if arduino_is_ready(line):
-        print('Received Ready signal again')
-        break
-    line = line.decode('utf-8')
-    line = line.replace("\r\n","")
-    print('Arduino says:', line)
+last_vertical = ''
+minV = 30
+maxV = 140
+rangeV = maxV - minV
+progress = tqdm.tqdm(range(100))
 
-print('Gracefully closing serial port')
+with open(sys.argv[1], 'w') as f:
+    while ser.is_open:
+        line = ser.readline()
+        if arduino_is_ready(line):
+            logger.info('Received Ready signal again')
+            break
+        line = line.decode('utf-8')
+        line = line.replace("\r\n","")
+        parts = line.split(" ")
+        vertical = parts[0]
+        if vertical[0] == 'V' and vertical != last_vertical:
+            v = int(vertical[1:])
+            percents = round(100 * ((rangeV - v + minV) / rangeV))
+            progress.update(percents)
+            #logger.info('%s (%d %%)', vertical, percents)
+            last_vertical = vertical
+        f.write(line)
+        f.write('\n')
+
+logger.info('Gracefully closing serial port')
 ser.close()
